9893b257d13efa4353134cb79004f199cc402839
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-07-23 18:07:20 +0800

    add 2 extra light on it

--- three_fiber/src/App.js
+++ three_fiber/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import "./style.css";
 import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
@@ -6,8 +6,10 @@ import { Ground } from './Ground';
 import { useFrame } from '@react-three/fiber';
 
 function CarShow(){
-  const spotlightref1 = React.useRef();
-  const spotlightref2 = React.useRef();
+  const spotlightref1 = useRef();
+  const spotlightref2 = useRef();
+  const spotlightref3 = useRef(); // New ref for green spotlight
+  const spotlightref4 = useRef(); // New ref for yellow spotlight
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime(); // Get the elapsed time
@@ -17,61 +19,60 @@ function CarShow(){
     spotlightref1.current.position.z = Math.sin(t) * 1 - 1;
     spotlightref2.current.position.x = -Math.cos(t) * 1 + 0;
     spotlightref2.current.position.z = -Math.sin(t) * 1 + 0;
-    // spotLight1Z = Math.sin(t) * 1;
-
+    spotlightref3.current.position.x = Math.cos(t) * 1 + 1; // Set position for green spotlight
+    spotlightref3.current.position.z = Math.sin(t) * 1 + 1;
+    spotlightref4.current.position.x = -Math.cos(t) * 1 - 1; // Set position for yellow spotlight
+    spotlightref4.current.position.z = -Math.sin(t) * 1 - 1;
   });
 
-  return(
+  return (
     <>
-      <OrbitControls target={[0,0.35,0]} maxPolarAngle={1.45}/>
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]}/>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      {/* init color */}
-        <color args={[0,0,0]} attach={"background"}/>
+      <color args={[0, 0, 0]} attach={"background"} />
 
-      {/* init spotcolor */}
-      <spotLight ref={spotlightref1} 
-        color={[1,0.25,0.7]}
+      <spotLight ref={spotlightref1}
+        color={[1, 0.25, 0.7]}
         intensity={1.5}
         angle={0.6}
         penumbra={0.5}
-        position={[0 ,5,0]}
+        position={[0, 5, 0]}
         castShadow
         shadowBias={-0.0001}
       />
 
-      <spotLight ref={spotlightref2} 
-        color={[0.14,0.5,1]}
-        intensity={2}
-        angle={0.6}
-        penumbra={0.5}
-        position={[-5,5,0]}
-        castShadow
-        shadowBias={-0.0001}
-      />
-
-      {/* New spotlights */}
-      {/* <spotLight
+      <spotLight ref={spotlightref2}
         color={[0.14, 0.5, 1]}
         intensity={2}
         angle={0.6}
         penumbra={0.5}
+        position={[-5, 5, 0]}
         castShadow
         shadowBias={-0.0001}
-        position={[5,5,0]} // Store a reference to the spotlight
       />
 
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
+      <spotLight ref={spotlightref3} // Add green spotlight
+        color={[0, 1, 0]}
+        intensity={1.5}
         angle={0.6}
         penumbra={0.5}
+        position={[0, 5, 0]}
         castShadow
         shadowBias={-0.0001}
-        position={[5,5,0]}
-      /> */}
+      />
 
-      <Ground/>
+      <spotLight ref={spotlightref4} // Add yellow spotlight
+        color={[1, 1, 0]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[0, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
+
+      <Ground />
     </>
   )
 }
