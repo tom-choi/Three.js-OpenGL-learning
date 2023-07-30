0391fbc89974be6c044cc35011d7ef5bc77e4c4e
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-07-30 16:33:44 +0800

    fbx import

--- three_fiber/src/App.js
+++ three_fiber/src/App.js
@@ -1,13 +1,37 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei";
-import "./style.css";
+import "./App.css";
+
+import { useLoader } from '@react-three/fiber';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+function Title(){
+  return (
+    <>
+      <div className="relative z-0">
+        <h1>HelloWorld!</h1>
+      </div>
+    </>
+  );
+}
+
+function Iphone(){
+  const fbx = useLoader(FBXLoader, './model/Iphone seceond version finished.fbx');
+  return(
+    <>
+      <mesh>
+        <primitive object={fbx} />
+      </mesh>
+    </>
+  )
+}
 
 function CarShow(){
   return(
     <>
       <OrbitControls target={[0,0.35,0]} maxPolarAngle={1.45}/>
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]}/>
+      <PerspectiveCamera makeDefault fov={50} position={[8, 2, 10]}/>
 
 
       <Plane args={[10, 10]}>
@@ -20,9 +44,7 @@ function CarShow(){
 function App() {
   return (
     <Suspense fallback={null}>
-      <div className='title'>
-        <h1>HelloWorld!</h1>
-      </div>
+      <Title />
       <Canvas style={{ background: "black" }} shadows>
         <CarShow />
       </Canvas>
